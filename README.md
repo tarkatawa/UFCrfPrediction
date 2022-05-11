5292ee0473712562ef2bfc449d7bc83450f96dff
omgede <64190947+omgede@users.noreply.github.com>
2022-05-11 11:58:01 +0700

    Modularize the code slightly

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Flask application to run my UFC Pred Machine Learning Model.
 ## TODO List
 
 - ~~Move training data to startup.~~
-- Modularize code.
+- ~~Modularize code.~~
 - Write validations in application level.
 - Deploy to the Internet.
 - Code quality improvements: `black`, `isort`, and `pylint`.
+- Package management? `poetry` or `requirements.txt`?
--- app.py
+++ app.py
@@ -4,53 +4,62 @@ from flask import Flask, jsonify, request
 from .random_forest import pretrain_model, random_forest
 
 # initial bootstrap, define app, pretrain model, and reuse the RF instance as global variable
-print("Training models, please wait before the app starts...")
-rf, data, accuracy = pretrain_model()
-print("Model has been successfully pretrained!")
+def create_app():
+    print(" * (RF) Training model, please wait before the app can start...")
+    rf, data, accuracy = pretrain_model()
 
-# start Flask app
-print ("Starting Flask app...")
-app = Flask(__name__)
+    # define app
+    print(" * (RF) Model has been successfully pretrained. Starting app...")
+    app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return jsonify(status="success", message="Hello, World!")
+    # with app context, do...
+    with app.app_context():
+        @app.route("/")
+        def hello():
+            return jsonify(status="success", message="Hello, World!")
 
-# Expected request body:
-# {
-#     "redFighter": "Dustin Poirier",
-#     "blueFighter": "Tony Ferguson",
-#     "redOdds": 120,
-#     "blueOdds": -120,
-#     "betterRank": 0,
-#     "numberOfRounds": 5
-# }
-@app.route("/predict", methods=["POST"])
-def predict():
-    # fetch from request body
-    red_fighter, blue_fighter, red_odds, blue_odds, better_rank, number_of_rounds = itemgetter("redFighter", "blueFighter", "redOdds", "blueOdds", "betterRank", "numberOfRounds")(request.json)
-    
-    # make prediction based on the trained model
-    result = random_forest(
-        red_fighter=red_fighter,
-        blue_fighter=blue_fighter,
-        red_odds=red_odds,
-        blue_odds=blue_odds,
-        better_rank=better_rank,
-        number_of_rounds=number_of_rounds,
-        rf=rf,
-        data=data,
-    )
+        # Expected request body:
+        # {
+        #     "redFighter": "Dustin Poirier",
+        #     "blueFighter": "Tony Ferguson",
+        #     "redOdds": 120,
+        #     "blueOdds": -120,
+        #     "betterRank": 0,
+        #     "numberOfRounds": 5
+        # }
+        @app.route("/predict", methods=["POST"])
+        def predict():
+            # fetch from request body
+            red_fighter, blue_fighter, red_odds, blue_odds, better_rank, number_of_rounds = itemgetter("redFighter", "blueFighter", "redOdds", "blueOdds", "betterRank", "numberOfRounds")(request.json)
+            
+            # make prediction based on the trained model
+            result = random_forest(
+                red_fighter=red_fighter,
+                blue_fighter=blue_fighter,
+                red_odds=red_odds,
+                blue_odds=blue_odds,
+                better_rank=better_rank,
+                number_of_rounds=number_of_rounds,
+                rf=rf,
+                data=data,
+            )
 
-    # encapsulate input in nested json
-    input = {
-        'redFighter': red_fighter,
-        'blueFighter': blue_fighter,
-        'redOdds': red_odds,
-        'blueOdds': blue_odds,
-        'betterRank': better_rank,
-        'numberOfRounds': number_of_rounds,
-    }
+            # encapsulate input in nested json
+            input = {
+                'redFighter': red_fighter,
+                'blueFighter': blue_fighter,
+                'redOdds': red_odds,
+                'blueOdds': blue_odds,
+                'betterRank': better_rank,
+                'numberOfRounds': number_of_rounds,
+            }
 
-    # return data as json, this will be read by the android app
-    return jsonify(prediction=result, input=input, accuracy=accuracy)
+            # return data as json, this will be read by the android app
+            return jsonify(prediction=result, input=input, accuracy=accuracy)
+
+    # return app instance
+    return app
+
+# in startup, start the 'create_app' function
+if __name__ =="__main__":
+    create_app()
