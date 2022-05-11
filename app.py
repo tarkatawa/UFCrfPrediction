0dffaeef6444f395380c016e307500ac054d121b
omgede <64190947+omgede@users.noreply.github.com>
2022-05-11 11:36:11 +0700

    Adjust webserver to accept prediction

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 from operator import itemgetter
 
 from flask import Flask, jsonify, request
+from .random_forest import random_forest
 
 app = Flask(__name__)
-
-def random_forest():
-    """
-    This function will perform data prediction based on the Google Colab script.
-    """
-    return True
 
 @app.route("/")
 def hello():
     return jsonify(status="success", message="Hello, World!")
 
-# Schema:
+# Expected request body:
 # {
-#     "redFighter": "Random Red Fighter",
-#     "blueFighter": "Random Blue Fighter",
-#     "redOdds": 100,
+#     "redFighter": "Dustin Poirier",
+#     "blueFighter": "Tony Ferguson",
+#     "redOdds": 120,
 #     "blueOdds": -120,
 #     "betterRank": 0,
-#     "numberOfRounds": 3
+#     "numberOfRounds": 5
 # }
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -29,7 +24,14 @@ def predict():
     red_fighter, blue_fighter, red_odds, blue_odds, better_rank, number_of_rounds = itemgetter("redFighter", "blueFighter", "redOdds", "blueOdds", "betterRank", "numberOfRounds")(request.json)
     
     # make prediction based on the trained model
-    result = random_forest()
+    result = random_forest(
+        red_fighter=red_fighter,
+        blue_fighter=blue_fighter,
+        red_odds=red_odds,
+        blue_odds=blue_odds,
+        better_rank=better_rank,
+        number_of_rounds=number_of_rounds
+    )
 
     # encapsulate input in nested json
     input = {
